3e8a73c881ae695c75ff0c350cb7cd1b78db5102
Edmund O'Brien <edobrien@Edmunds-MacBook-Air.local>
2015-06-27 11:10:19 +0200

    the set up

---
+++ js/app.js
@@ -0,0 +1,1 @@
+var myWallet = angular.module('MyWallet', ['ngResource']);
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var port = process.env.PORT || 8080
+var path = require('path');
+
+
+
+ app.use(express.static(__dirname + '/'));
+
+app.get('/', function(request, response){
+  response.sendfile('index.html')
+});
+
+
+
+server.listen(port, function(){
+  console.log("Server listening on port " + port);
+});
+
+module.exports = server;
